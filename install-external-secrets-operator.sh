44cd414ec37e2e38d1ad8d7d4b679f34ca6c4749
Tomasz-Kluczkowski <tomasz.kluczkowski@flexciton.com>
2022-08-11 13:44:08 +0100

    initial commit add basic setup script add k8s resources to test external secrets operator

---
+++ install-external-secrets-operator.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+helm repo add external-secrets https://charts.external-secrets.io
+
+helm install external-secrets \
+  external-secrets/external-secrets \
+  -n external-secrets \
+  --create-namespace \
+  --set installCRDs=true
